e97049f7bf6cc3d7afbb2acd43161ae7b89dfe0d
Subhrajyoti Sen <subhrajyoti12@gmail.com>
2016-06-04 22:39:23 +0530

    Initial commit

---
+++ jokesdisplay/src/main/java/com/subhrajyoti/jokesdisplay/JokeDisplay.java
@@ -0,0 +1,19 @@
+package com.subhrajyoti.jokesdisplay;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class JokeDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_joke_dsiplay);
+
+        Intent intent = getIntent();
+        String joke = intent.getStringExtra("joke");
+        Toast.makeText(this, joke, Toast.LENGTH_LONG).show();
+    }
+}
---
+++ jokesfactory/src/main/java/com/subhrajyoti/jokes/Jokes.java
@@ -0,0 +1,16 @@
+package com.subhrajyoti.jokes;
+
+import java.util.Random;
+
+public class Jokes {
+    public static String[] jokes = {"Chuck Norris can slam a revolving door",
+            "Chuck Norris once kicked a horse in the chin. Its descendants are known today as Giraffes",
+            "There is no theory of evolution. Just a list of animals Chuck Norris allows to live",
+            "There is no theory of evolution. Just a list of animals Chuck Norris allows to live",
+            "Chuck Norris can finish Mario Bros without using the jump button"
+    };
+
+    public String getJoke(){
+        return jokes[(new Random()).nextInt(jokes.length)];
+    }
+}
